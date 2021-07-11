Fixed equity calculation for Transactions.

backtester/model.py
@@ -33,7 +33,8 @@ from backtester.exceptions import DataError
 
 @dataclasses.dataclass
 class Action:
-    """
+    """State immediately **after** performance of an action. 
+    
     Parameters
     ----------
     date : datetime.datetime
@@ -64,7 +65,6 @@ class Action:
     fee : float = 0.0
     gain : float = 0.0
     price : float = None
-    
     
     def __post_init__(self):
         self.date = np.datetime64(self.date)
@@ -416,6 +416,9 @@ class SymbolTransactions:
         """Retrieve share value for a specific action index."""
         action = self.executed_actions[ii]
         shares =  action.shares
+        if shares == 0:
+            return 0.0
+        
         date = action.date
         date = floor_to_date(date)
         try:
@@ -493,6 +496,7 @@ class SymbolTransactions:
     
     
     def _check_dates(self, action: Action):
+        """Error check to make sure date is allowed."""
         date = action.date
         try:
             last_date= self.executed_actions[-1].date
@@ -503,6 +507,15 @@ class SymbolTransactions:
             s = (f'Transaction at {date} is not allowed on equal or before'
                  f' the last action at {last_date}.')
             raise TradingError(s)
+            
+        
+        # Check to make sure date is in the data index. 
+        date1 = np.datetime64(date, 'D')  
+        dates = self.df.index.values
+        loc = np.searchsorted(dates, date1)
+        
+        if dates[loc] != date1:
+            raise TradingError(f'{date} is not a valid trading day.')
         
     
     def _execute_one(self):
@@ -638,6 +651,10 @@ class Transactions:
         self.executed_actions = []
         self.balances = []
         self.price_name = price_name
+        
+        # Keep track of active stocks for latest action
+        self._latest_portfolio = set()
+        
         # self.stats = TransactionStats(self)
         
         if actions is None:
@@ -704,12 +721,29 @@ class Transactions:
             # Execute using add.
             st.add(action)
         return  
-                
+    
+    
+    def _check_action_timestamp(self, action: Action):
+        """Make sure new action is newer than previous actions."""
+        try:
+            last_action = self.executed_actions[-1]
+        except IndexError:
+            return
+        
+        last_date = last_action.date
+        if action.date <= last_date:
+            s = (f'Date for {action} '
+                 f'must be newer than last action at {last_date}.')
+            raise TradingError(s)
                     
+            
     def _execute_one(self):
         action = self.queue.popleft()
         name = action.name
         date = action.date
+        
+        # Check to make sure action has unique timestamp.
+        self._check_action_timestamp(action)
 
         try:
             last_balance: AccountBalance = self.balances[-1]
@@ -717,12 +751,11 @@ class Transactions:
             available_funds = self.init_funds
         else:
             available_funds = last_balance.available_funds
-        
+
         if name == ACTION_BUY:
             if available_funds - action.amount + SMALL_DOLLARS < 0:
                 action.amount = 0 
                 raise NoMoneyError(f'Available funds is less than 0 from {action}!')
-            
 
         self._execute_symbol(action)
 
@@ -735,12 +768,17 @@ class Transactions:
             
         elif name == ACTION_BUY:
             available_funds += -action.amount
+            self._latest_portfolio.add(action.symbol)
             
         elif name == ACTION_SELL:
             available_funds += action.gain
+            if action.shares == 0:
+                self._latest_portfolio.remove(action.symbol)
             
         elif name == ACTION_SELL_PERCENT:
-            available_funds += action.gain              
+            available_funds += action.gain          
+            if action.shares == 0:
+                self._latest_portfolio.remove(action.symbol)
         
         elif name == ACTION_HOLD:
             pass
@@ -794,38 +832,21 @@ class Transactions:
     
     def _execute_last_equity(self):
         """Calculate equity (cash + assets) for the last transaction."""
-        # try:
-        #     last_executed_balance = self.balances[-2]
-        #     last_equity = self.last_executed_balance.equity
-        # except IndexError:
-        #     last_equity = self.init_funds
-        # TODO THIS FUNCTION IS BROKEN!!
-        # raise NotImplementedError('THIS FUNCTION IS BROKEN!!')
-        
         balance : AccountBalance
         balance = self.balances[-1]
-        action = self.executed_actions[-1]
         
-        try:
-            last_balance = self.balances[-2]
-            last_equity = last_balance.equity
-        except IndexError:
-            pass
-        
+        equity = balance.available_funds
         
         sdict = self._get_symbol_transactions_dict()
-        symbol = action.symbol
-        if symbol is not None:
-            s_transaction = sdict[action.symbol]        
-            values = s_transaction.get_share_valuation_for_action(-1)
-        else:
-            values = 0
-        equity = balance.available_funds
-        equity += values
+        portfolio = self._latest_portfolio
+        
+        for symbol in portfolio:
+            s_transaction = sdict[symbol]
+            value = s_transaction.get_share_valuation(balance.date)
+            equity += value
         
         balance.equity = equity
         return
-        
         
         
     def _execute_equity(self):
@@ -837,10 +858,9 @@ class Transactions:
         dates = np.array([a.date for a in actions])
         equity = np.array([b.available_funds for b in balances])
         
-        
         for s_transaction in sdict.values():
             values = s_transaction.get_share_valuations(dates)
-            equity = equity + values
+            equity += values
 
         for ii, balance in enumerate(balances):
             balance.equity = equity[ii]

backtester/tests/test_symbol_transactions.py
@@ -216,15 +216,38 @@ def test_return_ratio():
 
     
     
+def test_share_valuation():
+    symbol = 'SPY'
+    yahoo = YahooData()
+    
+    date = np.datetime64('2016-01-04')
+    small = np.timedelta64(1, 'ms')
     
     
+    st = SymbolTransactions(symbol, yahoo)
+    dates = []
+    for ii in range(10):
+        dates.append(date)
+        if ii % 2 == 0:
+            action = Action(date, ACTION_BUY, amount=10, symbol=symbol)
+        else:
+            action = Action(date, ACTION_SELL, amount=10, symbol=symbol)
+        st.add(action)
+        date += small
+        
+        
+    dates = np.array(dates)
+    values1 = st.get_share_valuations(dates)
+    values2 = [st.get_share_valuation_for_action(ii) for ii in range(10)]
+    values2 = np.array(values2)
+    assert np.all(values1 == values2)
     
-
     
 if __name__ == '__main__':
-    # test_3()
-    # test_last_trading_date()
-    # test_last_trading_date2()
-    # test_exec_one()
-    # test_interday()
+    test_3()
+    test_last_trading_date()
+    test_last_trading_date2()
+    test_exec_one()
+    test_interday()
+    test_share_valuation()
     test_return_ratio()

backtester/tests/test_transactions.py
@@ -59,9 +59,7 @@ def test_transations():
     return
 
 def test_interday():
-    
-    
-        
+
     date1 = datetime.datetime(2017, 1, 5)
     date2 = datetime.datetime(2017, 1, 20, 0, 1)
     date3 = datetime.datetime(2017, 1, 20, 0, 2)
@@ -135,24 +133,65 @@ def test_equity2():
     
     date1 = np.datetime64('2016-01-01')
     dates = dates[dates > date1][0:5]
+    small = np.timedelta64(1, 'ms')
     
     transactions = Transactions(stock_data=yahoo, init_funds=100)
     for ii, date in enumerate(dates):
+        # Buy
         if ii % 2 == 0:
             for symbol in symbols:
                 transactions.buy(date, symbol, 10)
+                date += small
         else:
             for symbol in symbols:
                 transactions.sell_percent(date, symbol, 1)
+                date += small
                 
-    df = transactions.dataframe
-    pdb.set_trace()
+    df1 = transactions.dataframe
+    # pdb.set_trace()
+    del transactions.dataframe
+    transactions._execute_equity()
+    df2 = transactions.dataframe
     
+    
+    def check_column(name):
+        try:
+            assert np.all(np.isclose(df1[name], df1[name]))
+        except TypeError:
+            assert np.all(df1[name] == df2[name])
+    for column in df1.columns:
+        check_column(column)
+    
+    
+def test_equity3():
+    yahoo = YahooData()
+    symbols = ['SPY', 'GOOG', 'TSLA', 'AAPL',]
+    
+    date = np.datetime64('2016-01-04')
+    small = np.timedelta64(1, 'ms')
+
+    transactions = Transactions(stock_data=yahoo, 
+                                commission=0.0,
+                                init_funds=100)
+    for ii in range(10):
+        if ii % 2 == 0:
+            for symbol in symbols:
+                transactions.buy(date, symbol, 10)
+                date += small
+        else:
+            for symbol in symbols:
+                transactions.sell_percent(date, symbol, 1)
+                date += small
+                
+    df1 = transactions.dataframe
+    assert np.all(df1['equity'] == 100)
     
 
+
+
 if __name__ == '__main__':
-    # test_transations()
-    # test_interday()
-    # test_equity()
+    test_transations()
+    test_interday()
+    test_equity()
     test_equity2()
-    # test_shares()
+    test_equity3()